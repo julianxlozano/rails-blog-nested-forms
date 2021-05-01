42ee5039461ae5f67c2378da09b12284fb1917fe
julianxlozano <julianxlozano@gmail.com>
2021-05-01 13:04:55 -0500

    commit tree 1d56002e428c8ed6fcda4054c5df2afc21e092a3

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
+  accepts_nested_attributes_for :tags, reject_if: :blank
 
   validates_presence_of :name, :content
+
+  def blank(attributes)
+    attributes[:name].blank?
+end
 end
